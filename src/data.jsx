c3c446c615e848853e540ca374f1b6bf1fe3700a
Mykhailo Shevchenko <shevchenko21021984@gmail.com>
2024-02-21 19:13:02 +0100

    first commit

--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import { FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import { FaHtml5, FaYoutubeSquare, FaComments } from "react-icons/fa";
 
 export const links = [
   { id: nanoid(), href: "#home", text: "home" },
@@ -13,19 +13,19 @@ export const skills = [
     id: nanoid(),
     title: "HTML&CSS",
     icon: <FaHtml5 className="h-16 w-16 text-emerald-500" />,
-    text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
+    text: "Experienced in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
   },
   {
     id: nanoid(),
-    title: "Javascript",
-    icon: <FaJs className="h-16 w-16 text-emerald-500" />,
-    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
+    title: "YouTube",
+    icon: <FaYoutubeSquare className="h-16 w-16 text-emerald-500" />,
+    text: "Experienced YouTube user, creating interactive and dynamic web applications with a focus on smooth user interactions and functionality.",
   },
   {
     id: nanoid(),
-    title: "React",
-    icon: <FaReact className="h-16 w-16 text-emerald-500" />,
-    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+    title: "ChatGPT",
+    icon: <FaComments className="h-16 w-16 text-emerald-500" />,
+    text: "Familiar with ChatGPT, exploring innovative ways to integrate AI-powered chatbots into web applications for enhanced user experiences.",
   },
 ];
 
@@ -33,25 +33,25 @@ export const projects = [
   {
     id: nanoid(),
     img: "https://images.pexels.com/photos/326503/pexels-photo-326503.jpeg?auto=compress&cs=tinysrgb&w=800",
-    url: "https://menu-react-application.netlify.app/",
+    url: "https://youtu.be/mjNx17nLfiI",
     github: "https://github.com/john-smilga",
     title: "first project",
-    text: "The Menu app, built with React, simplifies the display and management of menu items. Users can explore categorized menu items effortlessly, with features like dynamic rendering and category filtering enhancing the browsing experience.",
+    text: "This is a video I edited about what I am interested in besides design and coding.",
   },
   {
     id: nanoid(),
     img: "https://images.pexels.com/photos/2148222/pexels-photo-2148222.jpeg?auto=compress&cs=tinysrgb&w=800",
-    url: "https://accordion-app-react-new.netlify.app/",
+    url: "https://www.youtube.com/shorts/GP9IZpCjg6k",
     github: "https://github.com/john-smilga",
     title: "second project",
-    text: "The Accordion project enables users to effortlessly navigate through a list of questions and answers using React. It offers interactive toggling functionality for each question, streamlining the exploration process.",
+    text: "This is a random photo I edited using IbisPaint X.",
   },
   {
     id: nanoid(),
     img: "https://images.pexels.com/photos/12883026/pexels-photo-12883026.jpeg?auto=compress&cs=tinysrgb&w=800",
-    url: "https://tours-projectreact.netlify.app",
+    url: "https://www.youtube.com/shorts/RLvAedyCBOI",
     github: "https://github.com/john-smilga",
     title: "third project",
-    text: `The Tours app, powered by React, simplifies tour management with features like fetching, viewing, and interacting with tour data, including a "read more" functionality, along with a "re-fetch" option for updated listings.`,
+    text: `This is a short and simple cartoon that I created.`,
   },
 ];
