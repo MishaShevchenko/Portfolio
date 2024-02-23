a850f1c8bd17ec005846bdaa45000560e1b3a1cc
Mykhailo Shevchenko <shevchenko21021984@gmail.com>
2024-02-23 10:57:17 +0100

    fix hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="bg-emerald-100 py-24">
       <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
-                  <h1 className="text-7xl font-bold tracking-wider">{`I'm Artem`}</h1>
+                  <h1 className="text-7xl font-bold tracking-wider">{`I'm Mykhailo`}</h1>
           <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
             Front-End Developer
           </p>
