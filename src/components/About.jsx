4da087c79395aee69053a17778f6fae373912585
Mykhailo Shevchenko <shevchenko21021984@gmail.com>
2024-08-31 16:11:12 +0200

    fix project section, and about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,15 +8,15 @@ const About = () => {
         <article>
           <SectionTitle text="about me" />
           <p className="text-slate-600 mt-8 leading-loose">
-           I am a frontend developer with a passion for crafting exceptional
-            user experiences. My expertise lies in HTML, CSS, SCSS, JavaScript,
-            and React, where I combine design aesthetics with technical
-            proficiency to create engaging interfaces. Practicing a
-            comprehensive approach to web development, I seamlessly blend
-            frontend development with Agile principles, insights from IT
-            support, and cybersecurity. This allows me to prioritize user
-            experience while ensuring the reliability and security of web
-            applications.
+            I am a full-stack developer with a passion for crafting exceptional
+            user experiences. My expertise spans HTML, CSS, SCSS, JavaScript,
+            React, Node.js, and MySQL, where I seamlessly combine design
+            aesthetics with technical proficiency to create engaging and robust
+            interfaces. Embracing a comprehensive approach to web development, I
+            integrate frontend and backend development with Agile principles,
+            drawing on insights from IT support and cybersecurity. This allows
+            me to prioritize user experience while ensuring the reliability,
+            security, and scalability of web applications.
           </p>
         </article>
       </div>
