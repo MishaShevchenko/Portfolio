e9ddd813d468ac3a638e0c4af20faf7199e250c0
Mykhailo Shevchenko <shevchenko21021984@gmail.com>
2025-01-25 18:27:02 +0100

    fix about section 5

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,40 +11,25 @@ const About = () => {
   ];
 
   return (
-    <motion.section
-      className="bg-white py-20"
-      id="about"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ amount: 0.5 }}
-      transition={{ duration: 0.8 }}
-    >
+    <section className="bg-white py-20" id="about">
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
         {/* Image Section */}
         <motion.img
           src={aboutSvg}
           alt="About illustration"
           className="w-full h-64"
-          initial={{ x: -100, opacity: 0 }} // Slide from left for larger screens
-          animate={{ x: 0, opacity: 1 }}
-          viewport={{ amount: 0.5, once: false }}
-          transition={{
-            duration: 1,
-            delay: 0.1,
-            ease: "easeOut",
-          }}
+          initial={{ x: -100, opacity: 0 }} // Start off-screen to the left
+          whileInView={{ x: 0, opacity: 1 }} // Slide to center when in view
+          viewport={{ amount: 0.5, once: false }} // Trigger based on scroll
+          transition={{ duration: 1 }}
         />
 
         {/* Text Section */}
         <motion.article
-          initial={{ x: 100, opacity: 0 }} // Slide from right for larger screens
-          animate={{ x: 0, opacity: 1 }}
+          initial={{ x: 100, opacity: 0 }} // Start off-screen to the right
+          whileInView={{ x: 0, opacity: 1 }} // Slide to center when in view
           viewport={{ amount: 0.5, once: false }}
-          transition={{
-            duration: 1,
-            delay: 0.2,
-            ease: "easeOut",
-          }}
+          transition={{ duration: 1, delay: 0.2 }}
           className="text-slate-600 mt-8 leading-loose space-y-4"
         >
           <SectionTitle text="about me" />
@@ -53,11 +38,8 @@ const About = () => {
               key={i}
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
-              viewport={{ amount: 0.5 }}
-              transition={{
-                delay: i * 0.2,
-                duration: 0.8,
-              }}
+              viewport={{ amount: 0.5, once: false }}
+              transition={{ delay: i * 0.2, duration: 0.8 }}
               className="text-lg md:text-xl"
             >
               {line}
@@ -65,7 +47,7 @@ const About = () => {
           ))}
         </motion.article>
       </div>
-    </motion.section>
+    </section>
   );
 };
 
