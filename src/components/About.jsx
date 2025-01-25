e1e58664cbce3b121683aa47a69634ad6dd549b5
Mykhailo Shevchenko <shevchenko21021984@gmail.com>
2025-01-25 20:54:47 +0100

    fix about block 1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,48 +11,25 @@ const About = () => {
   ];
 
   return (
-    <section className="bg-white py-20" id="about">
+    <section className="bg-white py-20 overflow-hidden" id="about">
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
         {/* Image Section */}
         <motion.img
           src={aboutSvg}
           alt="About illustration"
           className="w-full h-64"
-          initial={{
-            x: window.innerWidth >= 768 ? -100 : 0, // Slide only on larger screens
-            opacity: 0,
-          }}
-          whileInView={{
-            x: 0,
-            opacity: 1,
-          }}
-          viewport={{
-            amount: 0.5,
-            once: false,
-          }}
-          transition={{
-            duration: 1,
-          }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ amount: 0.5, once: false }}
+          transition={{ duration: 1 }}
         />
 
         {/* Text Section */}
         <motion.article
-          initial={{
-            x: window.innerWidth >= 768 ? 100 : 0, // Slide only on larger screens
-            opacity: 0,
-          }}
-          whileInView={{
-            x: 0,
-            opacity: 1,
-          }}
-          viewport={{
-            amount: 0.5,
-            once: false,
-          }}
-          transition={{
-            duration: 1,
-            delay: 0.2,
-          }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ amount: 0.5, once: false }}
+          transition={{ duration: 1, delay: 0.2 }}
           className="text-slate-600 mt-8 leading-loose space-y-4"
         >
           <SectionTitle text="about me" />
@@ -61,9 +38,7 @@ const About = () => {
               key={i}
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
-              viewport={{
-                amount: 0.5,
-              }}
+              viewport={{ amount: 0.5 }}
               transition={{
                 delay: i * 0.2,
                 duration: 0.8,
