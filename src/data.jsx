0bb2e47735519f2f28ad9c236506463f2657a2f7
Misha Shevchenko <shevchenko21021984@gmail.com>
2025-03-20 10:11:17 +0100

    fix project section

--- src/data.jsx
+++ src/data.jsx
@@ -30,28 +30,29 @@ export const skills = [
 ];
 
 export const projects = [
-  {
-    id: nanoid(),
-    img: "https://images.pexels.com/photos/326503/pexels-photo-326503.jpeg?auto=compress&cs=tinysrgb&w=800",
-    url: "https://contentful-cms-mike.netlify.app/",
-    github: "https://github.com/MishaShevchenko",
-    title: "Explore the E-commerce Demo",
-    text: "Each project reflects my continuous growth in both front-end and back-end development. I've gained experience in responsive design, working with APIs, and implementing dynamic content. To see more details and explore live demos of these projects, check the links below.",
-  },
+ 
   {
     id: nanoid(),
     img: "https://images.pexels.com/photos/2148222/pexels-photo-2148222.jpeg?auto=compress&cs=tinysrgb&w=800",
     url: "https://contentful-cms-mike.netlify.app/",
     github: "https://github.com/MishaShevchenko",
-    title: "Try the To-Do App Live",
-    text: "These projects represent different stages of my journey in web development, highlighting various levels of complexity and technical skill. From integrating third-party APIs to creating responsive, user-friendly designs, each project has taught me valuable lessons. For more information and live demonstrations, follow the links below.",
+    title: "Contentful CMS",
+    text: "A content management system (CMS) powered by Contentful and React. It dynamically fetches and displays content, making it easy to manage and update projects. The project showcases structured content retrieval and rendering with optimized performance.",
+  },
+  {
+    id: nanoid(),
+    img: "https://images.pexels.com/photos/326503/pexels-photo-326503.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "https://contentful-cms-mike.netlify.app/project/1zk7K33oX6i97fc95Q133k",
+    github: "https://github.com/MishaShevchenko",
+    title: "E-commerce",
+    text: "A full-featured e-commerce store built with React, Redux Toolkit, and Firebase. It includes product filtering, sorting, pagination, a shopping cart, authentication, and order management. Users can browse, add items to their cart, and securely place orders.",
   },
   {
     id: nanoid(),
     img: "https://images.pexels.com/photos/12883026/pexels-photo-12883026.jpeg?auto=compress&cs=tinysrgb&w=800",
-    url: "https://contentful-cms-mike.netlify.app/",
+    url: "https://contentful-cms-mike.netlify.app/project/2NepQ8lVGHYYnMNtTlmFNL",
     github: "https://github.com/MishaShevchenko",
-    title: "Read the Blog App Codebase",
-    text: `Through these projects, I have developed a solid foundation in both front-end and back-end technologies, exploring areas like API integration, dynamic data handling, and modern web development practices. To see the projects in action and learn more, please visit the links below.`,
+    title: "MixMaster",
+    text: `MixMaster lets users search for cocktails and view detailed drink info using the Cocktail DB API. Built with React Router for seamless navigation and React Query for caching, it delivers a smooth multi-page experience optimized for performance.`,
   },
 ];
